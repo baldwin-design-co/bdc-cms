e7d280ee0581147123e30b2af2aaf7c4f808019b
Ben Baldwin <benbaldwindesign@gmail.com>
2020-10-02 16:52:27 -0700

    🔨 Fix: imported functions into firebase

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -2,6 +2,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/storage';
 import 'firebase/auth';
+import 'firebase/functions';
 
 // eslint-disable-next-line no-restricted-globals
 if (location.hostname === 'localhost') {
