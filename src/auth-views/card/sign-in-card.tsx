500140e32a0154e26380d97521949b50e4c1f3d2
Ben Baldwin <benbaldwindesign@gmail.com>
2020-10-02 23:42:57 -0700

    🎨 Visual: Auth card styling

--- src/auth-views/card/sign-in-card.tsx
+++ src/auth-views/card/sign-in-card.tsx
@@ -1,39 +1,54 @@
 import React, { useContext, useState } from 'react';
 import { authContext } from '../../context/auth-context';
 import { Button } from 'bdc-components';
+import { TextField, useTheme } from '@material-ui/core';
 import './card.css';
 
 export const SignInCard = () => {
 	const { signIn: authenticate } = useContext(authContext);
 	const [ credentials, setCredentials ] = useState({ email: '', password: '' });
 	const [ error, setError ] = useState('');
+	const theme = useTheme()
 
 	const signIn = async (credentials: { email: string; password: string }) => {
 		try {
 			await authenticate(credentials);
 		} catch (error) {
-			setError(error);
+			setError(error.message);
 		}
 	};
 
 	return (
-		<div className="sign-in-card">
-			<h2>Sign In</h2>
+		<>
+			<div className="sign-in-card">
+				<h2>Sign In</h2>
 
-			<b>Email</b>
-			<input
-				type="text"
-				onChange={e => setCredentials({ ...credentials, email: e.target.value })}
-			/>
-			<b>Password</b>
-			<input
-				type="password"
-				onChange={e => setCredentials({ ...credentials, password: e.target.value })}
-			/>
+				<TextField
+					label='Email'
+					size='small'
+					fullWidth={true}
+					variant='outlined'
+					style={{ margin: '16px 0 16px' }}
+					onChange={e => {
+						setCredentials({ ...credentials, email: e.target.value })
+					}}
+				/>
 
-			<Button action={() => signIn(credentials)}>Sign In</Button>
+				<TextField
+					label='Password'
+					size='small'
+					fullWidth={true}
+					variant='outlined'
+					style={{ margin: '0 0 16px' }}
+					onChange={e => {
+						setCredentials({ ...credentials, password: e.target.value })
+					}}
+				/>
 
-			{error ? <span className="error-message">{error}</span> : null}
-		</div>
+				<Button action={() => signIn(credentials)}>Sign In</Button>
+			</div>
+
+			{error ? <span className="error-message" style={{ color: theme.palette.error.main }}>{error}</span> : null}
+		</>
 	);
 };
